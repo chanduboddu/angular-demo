a658952c89778487d68923f9431aa378d38c141b
chanduboddu <chandu.b943@gmail.com>
2020-03-02 16:05:09 +0530

    2 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpclientModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { PostsComponentComponent } from './posts-component/posts-component.component';
-import { DataServiceService } from './data-service.service';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,HttpclientModule] ,
-  declarations: [ AppComponent, HelloComponent, PostsComponentComponent ],
+  imports:      [ BrowserModule, FormsModule] ,
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [DataServiceService]
+  providers: []
 })
 export class AppModule { }
